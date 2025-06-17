794efa236d8c4bacf20fd6347ec17bee0a02a7b3
Rubber-DuckKing <22943@students.isb.ac.th>
2025-06-17 10:42:37 +0700

    Made the text fit better

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,7 @@ msg = "Hello world!"
 print(msg)
 print("Nice to meet you!")
 # Write something about what you did over the last break
-print("This past weekend I went to Ayutthaya. We saw a lot of temples and I learned a little more about what certain elements meant and what inspired people to do certain thinsg")
+print("This past weekend I went to Ayutthaya. " \
+"We saw a lot of temples and I learned a little more " \
+"about what certain elements meant and what " \
+"inspired people to do certain thinsg")
